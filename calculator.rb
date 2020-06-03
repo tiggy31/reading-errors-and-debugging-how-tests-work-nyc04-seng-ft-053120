d8dbc906088179ed81795c7f4635050531ca2853
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-03 15:03:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,2 @@
 # Add your variables here
 
-first_number = 0
-second_number = !0
-sum = first_number +second_number
-difference = first_number-second_number
-product = first_number * second_number
-quotient  = first_number/second_number
